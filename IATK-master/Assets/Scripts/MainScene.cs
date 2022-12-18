7f0ff9c6599b748f8b6ae9515ff8a997f612e29c
arnx18 <arnx18@gmail.com>
2022-12-18 21:15:28 +0100

    randomize mainscene

--- IATK-master/Assets/Scripts/MainScene.cs
+++ IATK-master/Assets/Scripts/MainScene.cs
@@ -1,11 +1,100 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.XR;
 using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
 
 public class MainScene : MonoBehaviour
 {
-    public void FinishButton() {
-        SceneManager.LoadScene("MainMenu");
+    public InputActionReference toggleReference = null;
+
+    private Boolean started = false;
+
+    private int currentVisualisation;
+
+    private Boolean triggerValue;
+
+    private Stopwatch sw;
+
+    private List<double> elapsedSecondsArray;
+
+    private String[] order;
+    private int[] ind;
+
+    private void Awake()
+    {   
+        toggleReference.action.started += Toggle;
     }
+
+    private void OnDestroy()
+    {
+        toggleReference.action.started -= Toggle;
+    }
+
+    private void Toggle(InputAction.CallbackContext context)
+    {
+        if (!started) {
+            StartTest();
+        } else {
+            NextVisualisation();
+        }
+    }
+
+    private void Start() {
+        order = new String[transform.childCount];
+        for (int i = 0; i < transform.childCount; ++i) {
+            order[i] = transform.GetChild(i).gameObject.name;
+        }
+        System.Random rnd = new System.Random();
+        ind = Enumerable.Range(0, transform.childCount).OrderBy(c => rnd.Next()).ToArray();
+        currentVisualisation = 0;
+    }
+
+    public void StartTest()
+    {
+        started = true;
+        elapsedSecondsArray = new List<double>();
+        SelectVisualisation(order[ind[currentVisualisation]]);
+        sw = new Stopwatch();
+        sw.Start();
+    }
+
+    private void SelectVisualisation(String name)
+    {
+        for (int i = 0; i < transform.childCount; ++i) {
+            transform.GetChild(i).gameObject.SetActive(transform.GetChild(i).gameObject.name.Equals(name));
+        }
+    }
+
+    public void NextVisualisation()
+    {
+        ++currentVisualisation;
+        sw.Stop();
+        elapsedSecondsArray.Add(sw.Elapsed.TotalSeconds);
+        if (currentVisualisation == transform.childCount) {
+            printTestResults();
+            SceneManager.LoadScene("MainMenu");
+        } else {
+            SelectVisualisation(order[ind[currentVisualisation]]);
+            sw = new Stopwatch();
+            sw.Start();
+        }
+    }
+
+    private void printTestResults()
+    {
+        String results = "";
+        for (int i = 0; i < transform.childCount; ++i) {
+            results += order[ind[i]] + ", " + String.Format("{0:0.0000}", elapsedSecondsArray[i]).Replace(",", ".") + "\n";
+        }
+        print(results);
+    }
+
 }
